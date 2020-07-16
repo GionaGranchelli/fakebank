da32f12fcb00ae3a5bc00f52a4a10590c468a9d8
GionaGranchelli <giona.granchelli@gmail.com>
2020-07-16 14:52:42 +0200

    update version of fileupload

---
+++ frontend/build/precache-manifest.cd81269402ee2c7b8205529f3abab804.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d6204b11d420f53d5cd8b5805b13ba4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1af602478c3d2658c4ca",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "97623aeabd7c7b1ea63b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1af602478c3d2658c4ca",
+    "url": "/static/js/2.cfc1ece3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.cfc1ece3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97623aeabd7c7b1ea63b",
+    "url": "/static/js/main.88401f0b.chunk.js"
+  },
+  {
+    "revision": "3546c613097e116b1c6b",
+    "url": "/static/js/runtime-main.7b01aa88.js"
+  }
+]);
